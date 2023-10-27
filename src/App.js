691e7568a32e54f875fcc00d00f5a02bdcc924ab
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-10-27 11:39:35 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ useEffect(() => {
     return;
   }
 
-  axios.get(`http://localhost:4000/verifyToken?token=${token}`).then(reponse => {
+  axios.get(`http://localhost:4000/verifyToken?token=${token}`).then(response => {
     setUserSession(response.data.token, response.data.user);
     setAuthLoading(false);
   }).catch(error => {
