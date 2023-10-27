0c9fb6f6568aa9d7d1579b0efce3a10eea9fab6d
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-10-27 11:37:24 +0700

    add app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import axios from 'axios';
+
+import PrivateRoutes from "./utils/PrivateRoutes";
+import PublicRoutes from "./utils/PublicRouters";
+import { getToken, removeUserSession, setUserSession } from "./utils/common";
+
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const [authLoading, setAuthLoading] = useState(true);
+
+useEffect(() => {
+  const token = getToken();
+  if (!token) {
+    return;
+  }
+
+  axios.get(`http://localhost:4000/verifyToken?token=${token}`).then(reponse => {
+    setUserSession(response.data.token, response.data.user);
+    setAuthLoading(false);
+  }).catch(error => {
+    removeUserSession();
+    setAuthLoading(false);
+  });
+}, []);
+
+if (authLoading && getToken()) {
+  return <div className="content">Checking Authentication...</div>
+}
+
+return ( 
+  <BrowserRouter>
+  <div className="header">
+    <NavLink className={({ isActive }) => isActive ? 'active' : ''} to="/"> Home</NavLink>
+    <NavLink className={({ isActive }) => isActive ? 'active' : ''}>Login</NavLink><small>(Access without token only)</small>
+    <NavLink className={({ isActive }) => isActive ? 'active' : ''}>Dashboard</NavLink><small>(Access with token only)</small>
     </div>
-  );
+    <div className="content">
+      <Routes>
+        <Route path="*" element={<NotFound /> }/>
+        <Route index element={<Home />} />
+        <Route element={<PublicRoutes />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+          <Route element={<PrivateRoutes />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Route>
+      </Routes>
+    </div>
+    </BrowserRouter>
+);
 }
 
 export default App;
